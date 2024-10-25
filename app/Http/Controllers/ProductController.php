ed4d58a68a7247684fa279f9f580020e7f14e7e3
Ashraful Islam Sohag <sohagali09876543@gmail.com>
2024-10-25 12:40:27 +0600

    table create

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+   public function product (Request $request) {
+       return view('product');
+   }
+}
---
+++ resources/views/product.blade.php
@@ -0,0 +1,48 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel= "stylesheet" href= "https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" >
+    <title>Ajax Crud Operation</title>
+  </head>
+  <body>
+       <div class="container">
+        <div class="row">
+            <div class="col-lg-2"></div>
+            <div class="col-lg-8 my-4">
+                <h2 class="text-center pb-2"> Laravel Ajax Crud Operation</h2>
+                <div class="table-data">
+                    <table class="table table-bordered">
+                        <thead>
+                          <tr>
+                            <th scope="col">Sl</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th >1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>
+                                <a href="" class="btn btn-success"><i class="las la-edit"></i></a>
+                                <a href="" class="btn btn-danger"><i class="las la-times"></i></a>
+                            </td>
+                          </tr>
+
+                        </tbody>
+                      </table>
+                </div>
+            </div>
+        </div>
+       </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</html>
